8130dd8306c084878eb0a1f6578f98188f3fca25
Javier Figueroa <109968115+javiernfigueroa@users.noreply.github.com>
2023-03-27 18:49:21 -0300

    feed.html

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
-import { tweetsData } from './data.js';
+import { tweetsData } from "./data.js";
 
-console.log(tweetsData)
+const tweetInput = document.getElementById("tweet-input");
+const tweetBtn = document.getElementById("tweet-btn");
+const feed = document.getElementById("feed")
+
+tweetBtn.addEventListener("click", function () {
+  console.log(tweetInput.value);
+});
+
+function getFeedHtml() {
+  let feedHtml = "";
+  for (let tweets of tweetsData) {
+    feedHtml += 
+    `<div class="tweet">
+    <div class="tweet-inner">
+        <img src="${tweets.profilePic}" class="profile-pic">
+        <div>
+            <p class="handle">${tweets.handle}</p>
+            <p class="tweet-text">${tweets.tweetText}</p>
+            <div class="tweet-details">
+                <span class="tweet-detail">
+                ${tweets.replies}
+                </span>
+                <span class="tweet-detail">
+                ${tweets.likes}
+                </span>
+                <span class="tweet-detail">
+                ${tweets.retweets}
+                </span>
+            </div>   
+        </div>            
+    </div>
+</div>
+`;
+  }
+  feed.innerHTML = feedHtml;
+}
+
+getFeedHtml();
