b1c5c378b70aa3e8f44961d7c429962d95d5650f
Javier Figueroa <109968115+javiernfigueroa@users.noreply.github.com>
2023-03-28 09:44:03 -0300

    color to heart and rt

--- index.js
+++ index.js
@@ -8,9 +8,55 @@ tweetBtn.addEventListener("click", function () {
   console.log(tweetInput.value);
 });
 
+document.addEventListener("click", function (e) {
+  if (e.target.dataset.like) {
+    handleLikeClick(e.target.dataset.like);
+  } else if (e.target.dataset.reply) {
+    console.log(e.target.dataset.reply);
+  } else if (e.target.dataset.retweet) {
+    handleRtClick(e.target.dataset.retweet);
+  }
+});
+
+function handleLikeClick(tweetId) {
+  const targetTweetObj = tweetsData.filter(function (tweet) {
+    return tweet.uuid === tweetId;
+  })[0];
+  if (targetTweetObj.isLiked) {
+    targetTweetObj.likes--;
+  } else {
+    targetTweetObj.likes++;
+  }
+  targetTweetObj.isLiked = !targetTweetObj.isLiked;
+  renderTweets();
+}
+
+function handleRtClick(tweetId) {
+  const targetTweetObj = tweetsData.filter(function (tweet) {
+    return tweet.uuid === tweetId;
+  })[0];
+  if (targetTweetObj.isRetweeted) {
+    targetTweetObj.retweets--;
+  } else {
+    targetTweetObj.retweets++;
+  }
+  targetTweetObj.isRetweeted = !targetTweetObj.isRetweeted;
+  renderTweets();
+}
+
 function getFeedHtml() {
   let feedHtml = "";
+
   tweetsData.forEach(function (tweets) {
+    let likeIconClass = "";
+    if (tweets.isLiked) {
+      likeIconClass = "liked";
+    }
+    let retweetIconClass = "";
+    if (tweets.isRetweeted) {
+      retweetIconClass = "retweeted";
+    }
+
     feedHtml += `<div class="tweet">
     <div class="tweet-inner">
         <img src="${tweets.profilePic}" class="profile-pic">
@@ -19,15 +65,15 @@ function getFeedHtml() {
             <p class="tweet-text">${tweets.tweetText}</p>
             <div class="tweet-details">
                 <span class="tweet-detail">
-                <i class="fa-regular fa-comment-dots"></i>           
+                <i class="fa-regular fa-comment-dots" data-reply="${tweets.uuid}"></i>           
                 ${tweets.replies.length}
                 </span>
                 <span class="tweet-detail">
-                <i class="fa-solid fa-heart"></i>
+                <i class="fa-solid fa-heart ${likeIconClass}" data-like="${tweets.uuid}"></i>
                 ${tweets.likes}
                 </span>
                 <span class="tweet-detail">
-                <i class="fa-solid fa-retweet"></i>
+                <i class="fa-solid fa-retweet ${retweetIconClass}" data-retweet="${tweets.uuid}"></i>
                 ${tweets.retweets}
                 </span>
             </div>   
